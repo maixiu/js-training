87bb310cb7dab6ae92cca50826e0fed048b13942
Matthieu Tabuteau <m.tabuteau@gmail.com>
2018-09-15 21:53:50 +0200

    keep.js

--- exercises/keep.js
+++ exercises/keep.js
@@ -11,10 +11,30 @@
  * and only keep 2 characters from the third character
  *
  */
+function keepFirst(s) {
+	return s.substring(0, 2);
+}
 
+function keepLast(s) {
+	return s.substring(s.length - 2);
+}
+
+function keepFirstLast(s) {
+	return s.substring(2, 4);
+}
 
 //* Begin of tests
 const assert = require('assert')
 
-assert.fail('You must write your own tests')
+assert.strictEqual(keepFirst('assert'), 'as');
+assert.strictEqual(keepFirst('a'), 'a');
+assert.strictEqual(keepFirst(''), '');
+
+assert.strictEqual(keepLast('assert'), 'rt');
+assert.strictEqual(keepLast('a'), 'a');
+assert.strictEqual(keepLast(''), '');
+
+assert.strictEqual(keepFirstLast('assert'), 'se');
+assert.strictEqual(keepFirstLast('ase'), 'e');
+assert.strictEqual(keepFirstLast(''), '');
 // End of tests */
